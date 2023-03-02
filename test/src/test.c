68ff848eddc44a42e607a0d3e712dfec39644d29
czl <1161583182@qq.com>
2023-03-03 00:41:35 +0800

    new file:   .vscode/tasks.json 	modified:   test/src/deleteduplicates 	new file:   test/src/test 	new file:   test/src/test.c

---
+++ test/src/test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#define ADD 0
+#define MUl 1
+
+int Sum(int type, int arg, ...)
+{
+    va_list ap;
+    va_start(ap, arg);
+    int result = 0;
+    if (type == ADD) {
+        for (int i = 0; i < arg; i++) {
+            result += va_arg(ap, int);
+        }
+    } else {
+        result = 1;
+        for (int i = 0; i < arg; i++) {
+            result *= va_arg(ap, int);
+        }  
+    }
+    va_end(ap);
+    return result;
+}
+
+int main()
+{
+    int sum;
+    sum = Sum(1, 5, 1, 2, 3, 4, 5);
+    printf("sum is [%d].\n", sum);
+    return 0;
+}
