ee504a47a7f048367fc3f4d3465dce37089f4b16
czl <1161583182@qq.com>
2023-03-15 22:20:21 +0800

    modified:   test/src/test.c

--- test/src/test.c
+++ test/src/test.c
@@ -24,22 +24,51 @@ int Sum(int type, int arg, ...)
     return result;
 }
 
+int* searchRange(int* nums, int numsSize, int target){
+    int* ret = malloc(sizeof(int) * 2);
+    if (nums == NULL || numsSize == 0) {
+        ret[0] = -1;
+        ret[1] = -1;
+        return ret;
+    }
+    int left = 0;
+    int right = numsSize - 1;
+    while (left < right) {
+        int mid = (left + right) / 2;
+        if (nums[mid] >= target) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    if (nums[left] != target) {
+        ret[0] = -1;
+        ret[1] = -1;
+        return ret;
+    }
+    ret[0] = left;
+    left = 0;
+    right = numsSize - 1;
+    while (left < right) {
+        int mid = (left + right + 1) / 2;
+        if (nums[mid] <= target) {
+            left = mid;
+        } else {
+            right = mid - 1;
+        }
+    }
+    ret[1] = right;
+    return ret;
+}
+
 /* test begin */
 int main()
 {
     int sum;
-    sum = Sum(0, 6, 1, 2, 3, 4, 5, 6);
-    printf("sum is [%d].\n", sum);
+    // sum = Sum(0, 6, 1, 2, 3, 4, 5, 6);
+    // printf("sum is [%d].\n", sum);
+    int nums[] = { 5, 7, 7, 8, 8, 10};
+    int *res = searchRange(nums, 6, 6);
+    printf("res is {%d, %d}", *res, *++res);
     return 0;
 }
-
-#define RESERT_NUM 4
-#define BI_EVENT_PARAM_NUM 8
-typedef struct {
-    char *sn;
-    char *prodId;
-    int proType;
-    int failCase;
-    int reverse[RESERT_NUM];
-} BiEventParam;
-
